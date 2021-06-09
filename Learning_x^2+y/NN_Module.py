Fixed an error in the accuracy calculation and made some minor changes.
This is the combined notebook now (Sirak's 2D histograms are omitted because they are not generalizable to higher dimensional functions).

Learning_x^2+y/NN_Module.py
@@ -12,13 +12,33 @@ from pandas import read_csv
 
 # Sets error calculations
 def abs_err(pred, act):
+    """
+    Returns the absolute error of two numbers.
+
+    Inputs: pred (float), act (float)
+
+    Outputs: the absolute error (float)
+    """
     return abs(pred-act)
 def rel_err(pred, act):
-    return abs_err(pred, act)/act
+    """
+    Returns the relative error of two numbers.
+
+    Inputs: pred (float), act (float)
+
+    Outputs: the relative error (float)
+    """
+    return abs(abs_err(pred, act)/act)
 
 # Check what proportion of the predictions falls within 0.01 absolute accuracy or 1% relative accuracy
 def accu_test(prediction, actual):
-    "Output 1 if prediction is accurate enough and 0 otherwise"
+    """
+    Tests if two numbers are close enough (0.01 absolute error or 1% relative error).
+
+    Inputs: prediction (float), actual (float)
+
+    Outputs: 1 if close enough, 0 if not close enough (integer)
+    """
     if (abs_err(prediction, actual) < 0.01 or rel_err(prediction, actual) < 0.01):
         return 1
     else: return 0
@@ -26,6 +46,13 @@ v_accu_test = np.vectorize(accu_test) # This makes a vector function for conveni
 
 # Create a simple neural network with layer and node variabliltiy
 def create_model(inputs, outputs, hidden_nodes=100, layer_num = 0):
+    """
+    Creates a sequential model with the same number of nodes in each hidden layer.
+
+    Inputs: inputs (Pytorch tensor), outputs (Pytorch tensor), hidden_nodes (integer), layer_num (integer)
+
+    Outputs: model (Pytorch sequential container)
+    """
     layers = [torch.nn.Linear(inputs.shape[1],hidden_nodes),torch.nn.ReLU()]
     for i in range(layer_num):
         layers.append(torch.nn.Linear(hidden_nodes,hidden_nodes))
@@ -34,4 +61,4 @@ def create_model(inputs, outputs, hidden_nodes=100, layer_num = 0):
     model = torch.nn.Sequential(*layers)
     # include different number of nodes per layer functionality
     #list with nodes per layer
-    return (model.cuda(), hidden_nodes, layer_num)
+    return model.cuda()